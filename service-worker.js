dfc252d6f2a2b42e6fc167c8f35e218bdd089393
motoko104 <motoko104@yahoo.com>
2019-10-09 00:18:39 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/motoko104/CardCaptor_ClickyGame/precache-manifest.3029e23253615f2078fc9967a197f2a3.js"
+  "/motoko104/CardCaptor_ClickyGame.git/precache-manifest.5c39c26c161084626504e862d8136e18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/motoko104/CardCaptor_ClickyGame/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/motoko104/CardCaptor_ClickyGame.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
